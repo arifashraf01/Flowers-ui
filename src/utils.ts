405e7bb4b852feea4987209615d8cc8c5e20c7fa
ARIF\arifa <arif18052@gmail.com>
2026-07-08 21:51:22 +0530

    feat: add capitalize utility

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
 export const clsx = (...classes: string[]) => {
   return classes.filter(Boolean).join(' ');
 };
+
+export const capitalize = (str: string) => {
+  if (!str) return '';
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
